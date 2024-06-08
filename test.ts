8109bf2c3154a01fff126056754c83c72ebcb7fe
Vaspera <110002885+Vazpera@users.noreply.github.com>
2024-06-08 22:48:10 +0000

    What?

--- test.ts
+++ test.ts
@@ -7,7 +7,7 @@ interface hasName {
 }
 
 async function main() {
-    let x = { ...{ name: "john" }, ...{ pos: [1, 2, 3] } };
+    let x: hasPos | hasName = { ...{ name: "john" }, ...{ pos: [1, 2, 3] } };
 
     console.log(x);
 }
